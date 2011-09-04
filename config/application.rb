a9756604fd4b475b67b3e394905bbaa512dba2f3
Oriol Gual <oriol.gual@gmail.com>
2011-09-04 13:23:02 +0200

    update config to 3.1 asset pipeline

--- config/application.rb
+++ config/application.rb
@@ -2,9 +2,12 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
-# If you have a Gemfile, require the gems listed there, including any gems
-# you've limited to :test, :development, or :production.
-Bundler.require(:default, Rails.env) if defined?(Bundler)
+if defined?(Bundler)
+  # If you precompile assets before deploying to production, use this line
+  # Bundler.require *Rails.groups(:assets => %w(development test))
+  # If you want your assets lazily compiled in production, use this line
+  Bundler.require(:default, :assets, Rails.env)
+end
 
 module Silvia
   class Application < Rails::Application
@@ -29,7 +32,7 @@ module Silvia
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
+    config.i18n.default_locale = :ca
 
     # Please note that JavaScript expansions are *ignored altogether* if the asset
     # pipeline is enabled (see config.assets.enabled below). Put your defaults in
@@ -50,5 +53,6 @@ module Silvia
 
     # Enable the asset pipeline
     config.assets.enabled = true
+    config.assets.manifest = Rails.root.join('app/assets')
   end
 end
