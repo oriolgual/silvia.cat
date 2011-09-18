40f6c74773ead86134429190083eff7e49b2bbca
Oriol Gual <oriol.gual@gmail.com>
2011-09-18 21:42:22 +0200

    bare cropping works

--- app/models/illustration.rb
+++ app/models/illustration.rb
@@ -20,4 +20,28 @@ class Illustration < ActiveRecord::Base
   #   { x1: 0, y1: 0, x2: 100, y2: 100, width: 100, height: 100 }
   #
   serialize :thumbnail_coordinates, Hash
+
+  # Sets the thumbnail_attributes
+  #
+  # @param value If it's a JSON string it will be parsed as a Hash
+  def thumbnail_coordinates=(value)
+    if value.is_a?(String)
+      value = ActiveSupport::JSON.decode(value)
+    end
+    self[:thumbnail_coordinates] = value
+  end
+
+  def can_be_cropped?
+    thumbnail_coordinates.present? &&
+    thumbnail_coordinates['x'].to_i > 0 &&
+    thumbnail_coordinates['y'].to_i > 0 &&
+    thumbnail_coordinates['h'].to_i > 0 &&
+    thumbnail_coordinates['w'].to_i > 0
+  end
+
+  after_update :thumbnail!
+
+  def thumbnail!
+    image.recreate_versions! if thumbnail_coordinates_changed?
+  end
 end
--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -5,6 +5,27 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   storage :file
   # storage :fog
+  process :resize_to_limit => [480, 'X']
+
+  version :thumbnail do
+    process :crop
+    process resize_to_fill:  [100, 100]
+  end
+
+  def crop
+    return unless model.can_be_cropped?
+    manipulate! do |image|
+      x = model.thumbnail_coordinates['x'].to_i
+      y = model.thumbnail_coordinates['y'].to_i
+      height = model.thumbnail_coordinates['h'].to_i
+      width = model.thumbnail_coordinates['w'].to_i
+
+      image.combine_options do |cmd|
+        cmd.crop("#{width}x#{height}+#{x}+#{y}")
+      end
+      image
+    end
+  end
 
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
