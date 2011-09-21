fa27ea97608684f3acb39a987d54469413eb6ee6
Oriol Gual <oriol.gual@gmail.com>
2011-09-21 21:38:21 +0200

    tweak flash message steps to match iain backend

--- features/step_definitions/helper_steps.rb
+++ features/step_definitions/helper_steps.rb
@@ -4,6 +4,10 @@ def current_path
   URI.parse(current_url).path
 end
 
+def must_be_on(path)
+  current_path.must_equal path
+end
+
 def check_email_with_subject(options)
   unread_emails_for(options[:email]).select { |m| m.subject =~ Regexp.new(options[:subject]) }.size.must_equal(options[:count] || 1)
 end
@@ -18,10 +22,10 @@ end
 
 def must_have_message(type, message = nil)
   if message
-    within("div#flash-#{type}") do
+    within("div.flash .#{type}") do
       page.has_content?(message).must_equal true
     end
   else
-    page.has_css?("div#flash-#{type}").must_equal true
+    page.has_css?("div.flash .#{type}").must_equal true
   end
 end
