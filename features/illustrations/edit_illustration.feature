e2b24dc308014f3ea90a7944edcff43bbbb2193e
Oriol Gual <oriol.gual@gmail.com>
2011-09-24 20:39:59 +0200

    admin edits illustrations

--- features/illustrations/edit_illustration.feature
+++ features/illustrations/edit_illustration.feature
@@ -1,4 +1,4 @@
-Feature: Edt illustrations
+Feature: Edit illustrations
   As an admin
   I want to edit an illustration
   So I can change its data and create thumbnails
@@ -8,12 +8,8 @@ Feature: Edt illustrations
     And an illustration exists
 
   Scenario: Edit an illustration
-    Given I am add the illustration edit page
+    Given I am on the illustration edit page
     When I fill in the illustration form to change some data
     Then I should see a confirmation message telling me that the illustration has been updated
-    And I should be the changes
-
-  Scenario: Set a thumbnail
-    Given I am add the illustration edit page
-    When I set the thubmanil dimensions
-    Then I should see a thumbnail of the illustration image
+    And I should see the changes
+    And I should see a thumbnail of the illustration image
--- features/step_definitions/illustration_steps.rb
+++ features/step_definitions/illustration_steps.rb
@@ -6,10 +6,11 @@ Given /^I am add the illustrations create page$/ do
 end
 
 When /^I fill the illustration form with all the necessary data$/ do
-  fill_in 'Nom', with: 'Course fo witches'
+  fill_in 'Nom', with: 'Course of witches'
   fill_in 'Descripció', with: 'Personatge per un còmic'
   attach_file 'Imatge', Rails.root.join('spec/support/files/illustration.jpg')
   select(@current_tag.name, from: 'Tags')
+  find('#illustration_thumbnail_coordinates').set('{"x":0,"y":0,"w":10,"h":10}')
   click_button 'Crear Il·lustració'
 end
 
@@ -19,4 +20,30 @@ end
 
 Then /^I should be on the edit illustration page so I can create a thumbnailer later$/ do
   must_be_on edit_backend_illustration_path(Illustration.last)
+end
+
+Given /^an illustration exists$/ do
+  @current_illustration = Illustration.make!
+end
+
+Given /^I am on the illustration edit page$/ do
+  visit edit_backend_illustration_path(@current_illustration)
+end
+
+When /^I fill in the illustration form to change some data$/ do
+  fill_in 'Nom', with: 'Sketch #1'
+  find('#illustration_thumbnail_coordinates').set('{"x":0,"y":0,"w":10,"h":10}')
+  click_button 'Actualitzar Il·lustració'
+end
+
+Then /^I should see a confirmation message telling me that the illustration has been updated$/ do
+  must_have_confirmation_message('Il·lustració actualitzada correctament')
+end
+
+Then /^I should see the changes$/ do
+  page.has_content?('Sketch #1').must_equal true
+end
+
+Then /^I should see a thumbnail of the illustration image$/ do
+  page.has_css?('img#thumb').must_equal true
 end
