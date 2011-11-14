96137ac7f9d3735b04e24e00a7c4c0edaa98a8bd
Oriol Gual <oriol.gual@gmail.com>
2011-11-14 19:02:24 +0100

    install fancybox

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,3 +1,7 @@
 //= require jquery
 //= require jquery_ujs
+//= require fancybox
 //= require_tree .
+$(document).ready(function() {
+  $("#content_large_image .vertical_large_img a").fancybox();
+});
