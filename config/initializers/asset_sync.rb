266a2dd83c1b89648f71c09d5e22dad57e6342b4
Oriol Gual <oriol.gual@gmail.com>
2011-12-14 20:44:05 +0100

    update asset_sync config

--- config/initializers/asset_sync.rb
+++ config/initializers/asset_sync.rb
@@ -1,12 +1,22 @@
 if Rails.env.production?
   AssetSync.configure do |config|
     config.fog_provider = 'AWS'
-    config.fog_directory = ENV['FOG_DIRECTORY']
     config.aws_access_key_id = ENV['AWS_ACCESS_KEY_ID']
     config.aws_secret_access_key = ENV['AWS_SECRET_ACCESS_KEY']
+    config.fog_directory = ENV['FOG_DIRECTORY']
     config.fog_region = ENV['AWS_REGION']
+
+    # Don't delete files from the store
+    config.existing_remote_files = "keep"
+    #
+    # Automatically replace files with their equivalent gzip compressed version
     config.gzip_compression = true
-    config.manifest = true
+    #
+    # Use the Rails generated 'manifest.yml' file to produce the list of files to
+    # upload instead of searching the assets directory.
+    # config.manifest = true
+    #
+    # Fail silently.  Useful for environments such as Heroku
     config.fail_silently = true
   end
 end
