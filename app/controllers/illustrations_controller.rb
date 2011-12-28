a84eeb2649322d514d253274fc2773f1f633b9f7
Oriol Gual <oriol.gual@gmail.com>
2011-12-28 14:00:56 +0100

    pjax stuff for illustrations controller

--- app/controllers/illustrations_controller.rb
+++ app/controllers/illustrations_controller.rb
@@ -5,13 +5,29 @@ class IllustrationsController < ApplicationController
   actions :index, :show
   belongs_to :category, optional: true
   layout :select_layout
+  helper_method :elements_per_slice
+
+  def show
+    unless pjax_request?
+      @illustrations = Illustration.all
+    end
+    show!
+  end
 
   private
   def select_layout
-    if request.headers['X-PJAX']
+    if pjax_request?
       false
     else
       'application'
     end
+  end
+
+  def pjax_request?
+    request.headers['X-PJAX']
+  end
+
+  def elements_per_slice
+    8
   end
 end
