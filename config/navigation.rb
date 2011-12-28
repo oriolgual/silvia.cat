3bbfdae9d50626b376af9b79c94cff7f619c2fb4
Oriol Gual <oriol.gual@gmail.com>
2011-12-28 11:28:16 +0100

    add simple-navigation

---
+++ config/navigation.rb
@@ -0,0 +1,7 @@
+SimpleNavigation::Configuration.run do |navigation|
+  navigation.items do |primary|
+    Category.active.each do |category|
+      primary.item category.to_param, category.name, url_for(controller: 'illustrations', action: 'index', category_id: category), highlights_on: :subpath
+    end
+  end
+end
