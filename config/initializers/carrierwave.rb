2ffa4961661cc548def5a650881771309ba1bd0e
Oriol Gual <oriol.gual@gmail.com>
2011-12-30 20:20:49 +0100

    add fog host config

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,18 +3,19 @@ CarrierWave.configure do |config|
   if Rails.env.production?
     config.storage = :fog
     config.fog_credentials = {
-      :provider               => ENV['FOG_PROVIDER'],
-      :aws_access_key_id      => ENV['AWS_ACCESS_KEY_ID'],
-      :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY'],
-      :region                 => ENV['AWS_REGION']
+      provider:               ENV['FOG_PROVIDER'],
+      aws_access_key_id:      ENV['AWS_ACCESS_KEY_ID'],
+      aws_secret_access_key:  ENV['AWS_SECRET_ACCESS_KEY'],
+      region:                 ENV['AWS_REGION']
     }
     config.fog_directory  = ENV['FOG_BUCKET']
-    # config.fog_host       = 'https://assets.example.com'            # optional, defaults to nil
-    # config.fog_public     = true                                   # optional, defaults to true
-    config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
+    config.fog_host       = ENV['FOG_HOST']
+    config.fog_public     = true
+    config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}
   else
     config.storage = :file
     CarrierWave.root = Rails.root.join(Rails.public_path).to_s
   end
+
   config.cache_dir = "#{Rails.root}/tmp/uploads"
 end
