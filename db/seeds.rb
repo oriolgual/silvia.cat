4438d61e2c71df783bbe939b7e064762c3aa17c2
Oriol Gual <oriol.gual@gmail.com>
2012-01-01 20:10:17 +0100

    update seeds import

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,52 +1,22 @@
 # encoding: utf-8
 
 def rails_root
-  File.expand_path(File.dirname(__FILE__))
+  File.expand_path(File.dirname(File.dirname(__FILE__)))
 end
 
-Category.destroy_all
-Illustration.destroy_all
-
-["Els tresors d'Asgard", "Encàrrecs", "Fanart", "Galeria de dissenys", "Il·lustracions", "Naturales", "Retrats"].each do |name|
-  Category.create!(name: name, active: true)
+def seeds_path
+  rails_root + "/db/seeds/"
 end
 
-fanart_category = Category.find_by_name('Fanart')
-galeria_category = Category.find_by_name('Galeria de dissenys')
+categories = Dir.glob(seeds_path + '*').map {|path| Category.create!(name: path.split('/').last, active: true)}
 
-fanart = []
-fanart << { name:  'Inuyasha',
-                   description: 'Llapis i Gouache sobre cartulina.',
-                   image:       File.open(rails_root + '/seeds/Inuyasha012.jpg') }
-fanart << { name:  'Kagome "Inuyasha"',
-                   description: 'Llapis i Gouache sobre cartulina.',
-                   image:       File.open(rails_root + '/seeds/Kagome010.jpg') }
-fanart << { name:  'Kanna "Inuyasha"',
-                   description: 'Llapis i Gouache sobre cartulina.',
-                   image:       File.open(rails_root + '/seeds/img007.jpg') }
-fanart << { name:  'Katara',
-                   description: 'Avatar the last airbender',
-                   image:       File.open(rails_root + '/seeds/katarafiresd.jpg') }
-fanart << { name:  'Katara',
-                   description: 'Avatar the last airbender',
-                   image:       File.open(rails_root + '/seeds/katarasd.jpg') }
-fanart << { name:  'Dorothy i Espantaocells',
-                   description: 'OZ Disseny de personatges',
-                   image:       File.open(rails_root + '/seeds/oz-D.E..jpg') }
-fanart << { name:  'Dorothy i Espantaocells',
-                   description: 'OZ',
-                   image:       File.open(rails_root + '/seeds/Dorothy-and-scarecrowP2.jpg') }
-fanart << { name:  'Mofly',
-                   description: 'Mofly el último Koala',
-                   image:       File.open(rails_root + '/seeds/MOFLI.jpg') }
-fanart << { name:  'Katara',
-                   description: 'Avatar the last airbender (aquarel·la)',
-                   image:       File.open(rails_root + '/seeds/water_katara_millor_qualitat_by_silviabrujas.jpg') }
-
-fanart.each do |illustration|
-  Illustration.create!(name: illustration[:name],
-                      description: illustration[:description],
-                      image: illustration[:image],
-                      category: fanart_category,
-                      thumbnail_coordinates: { x: 100, y: 100, h: 100, w: 100 })
+categories.each do |category|
+  Dir.glob(seeds_path + category.name + '/*.jpg').each do |image|
+    name = image.split('/').last.split('.').first
+    Illustration.create!(name: name,
+                        description: name,
+                        image: File.open(image),
+                        category: category,
+                        thumbnail_coordinates: { x: 100, y: 100, h: 100, w: 100 })
+  end
 end
