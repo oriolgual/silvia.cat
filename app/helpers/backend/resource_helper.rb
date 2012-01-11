dfd3f0eb339c0a513762f5ef4748db49f23d5230
Oriol Gual <oriol.gual@gmail.com>
2012-01-11 21:04:38 +0100

    better illustration cropping

--- app/helpers/backend/resource_helper.rb
+++ app/helpers/backend/resource_helper.rb
@@ -2,4 +2,12 @@ module Backend::ResourceHelper
   def attributes
     resource_class.attribute_names - %w(id created_at updated_at thumbnail_coordinates)
   end
+
+  def select_current_coordinates(coordinates)
+    x = coordinates[:x]
+    y = coordinates[:y]
+    x2 = coordinates[:x].to_i + coordinates[:h].to_i
+    y2 = coordinates[:x].to_i + coordinates[:w].to_i
+    [x, y, x2, y2].join(', ')
+  end
 end
