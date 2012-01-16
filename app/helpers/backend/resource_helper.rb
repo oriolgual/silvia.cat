910f0846aacd993f3cb089c6038b825d025b617d
Oriol Gual <oriol.gual@gmail.com>
2012-01-16 20:55:14 +0100

    coordinates keys are strongs, not symbols

--- app/helpers/backend/resource_helper.rb
+++ app/helpers/backend/resource_helper.rb
@@ -4,10 +4,10 @@ module Backend::ResourceHelper
   end
 
   def select_current_coordinates(coordinates)
-    x = coordinates[:x]
-    y = coordinates[:y]
-    x2 = coordinates[:x].to_i + coordinates[:h].to_i
-    y2 = coordinates[:x].to_i + coordinates[:w].to_i
+    x = coordinates['x']
+    y = coordinates['y']
+    x2 = coordinates['x'].to_i + coordinates['h'].to_i
+    y2 = coordinates['x'].to_i + coordinates['w'].to_i
     [x, y, x2, y2].join(', ')
   end
 end
