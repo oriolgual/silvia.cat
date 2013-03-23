bbc6c0e47b5bdad864952ad0e21290aa8304dbd4
Oriol Gual <oriol.gual@gmail.com>
2013-03-23 11:28:29 +0100

    Update unicorn config

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,2 +1,23 @@
-worker_processes 2
-timeout 120
+worker_processes ENV.fetch('UNICORN_PROCESSES', 3).to_i
+timeout 30
+preload_app true
+listen ENV['PORT'], backlog: Integer(ENV['UNICORN_BACKLOG'] || 200)
+
+before_fork do |server, worker|
+  Signal.trap 'TERM' do
+    puts 'Unicorn master intercepting TERM and sending myself QUIT instead'
+    Process.kill 'QUIT', Process.pid
+  end
+
+  defined?(ActiveRecord::Base) and
+    ActiveRecord::Base.connection.disconnect!
+end
+
+after_fork do |server, worker|
+  Signal.trap 'TERM' do
+    puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to sent QUIT'
+  end
+
+  defined?(ActiveRecord::Base) and
+    ActiveRecord::Base.establish_connection
+end
