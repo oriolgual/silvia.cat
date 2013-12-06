a219ee217e3fb9a183199a2694dd9d1fa08d9186
Oriol Gual <oriol.gual@gmail.com>
2013-12-06 21:36:01 +0100

    Better URLs

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Silvia::Application.routes.draw do
 
-  resources :illustrations, only: :index do
-    get '(/category/:category_id)(/page/:page)', action: :index, on: :collection
-  end
   resources :videos, only: :index
 
   resource :contact_form, as: :contact, only: [:new, :create]
@@ -17,6 +14,9 @@ Silvia::Application.routes.draw do
   get '/resumee', to: redirect('https://s3-eu-west-1.amazonaws.com/silvia.cat/silvia_brujas_resumee.pdf')
   get '/resume', to: redirect('https://s3-eu-west-1.amazonaws.com/silvia.cat/silvia_brujas_resumee.pdf')
   get '/curriculum', to: redirect('https://s3-eu-west-1.amazonaws.com/silvia.cat/cv_silvia_brujas.pdf')
+
+  get '/illustrations/category/:category_id', to: redirect('/%{category_id}')
+  get '/:category_id', to: 'illustrations#index'
 
   root to: 'illustrations#index'
 end
