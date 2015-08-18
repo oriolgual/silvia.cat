ff6ae01532e443d21cfd1550f1c24149bcf43694
Oriol Gual <oriol.gual@gmail.com>
2015-08-18 11:41:34 +0200

    Only redirect if we could create the illustration

--- app/controllers/backend/illustrations_controller.rb
+++ app/controllers/backend/illustrations_controller.rb
@@ -3,6 +3,8 @@ class Backend::IllustrationsController < Backend::ResourceController
   # Overwrite IR create action to redirect to edit after create
   #
   def create
-    create! { edit_backend_illustration_path(resource) }
+    create! do |success, failure|
+      success.html { redirect_to edit_backend_illustration_path(resource) }
+    end
   end
 end
